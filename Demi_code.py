c211fe398f2cd0e9d1726fd604e32385ea1a40f7
rahulpatil <rahulpatil0106@gamil.com>
2022-09-05 10:37:37 +0530

    OPPs added

---
+++ Demi_code.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class Vehicle:
+
+    tyre = 'tubeless'
+
+    def __init__(self, brand, model, type):
+        self.brand = brand
+        self.model = model
+        self.type = type
+        self.gas_tank_size = 16
+        self.fuel_level = 0
+
+    def fuel_up(self):
+        self.fuel_level = self.gas_tank_size
+        print('Gas tank is now full.')
+
+    def drive(self):
+        print(f'The {self.model} is now driving.')
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(Vehicle.tyre)
+
+    vehicle_obj = Vehicle('BMW', 'C Series', 'Truck')
+
+    print(vehicle_obj.brand)
+    print(vehicle_obj)
+    print(vehicle_obj.type)
+
+    vehicle_obj.fuel_up()
+    vehicle_obj.drive()
+
--- demo_file.py
+++ demo_file.py
@@ -16,3 +16,49 @@ print(a)
 data= pd.DataFrame({"name":[1,2,3,4,5]})
 print(data)
 
+
+
+###  list
+
+a =["a",1,2,3,"AB"]
+print(a)
+
+## dictionary
+d = {"name":12,2:"Rahul"}
+print(d)
+
+##  set
+s = set([12,"rahul",45])
+s ={12,'rahul',45}
+print(s)
+
+## tuple
+tup  = (12,"Rahul",45)
+print(tup)
+print(type(tup))
+
+
+### - ------------ OPPs ----------------------##
+
+## Class
+class Man:
+
+     def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        #self.height =height
+        #self.weight =weight
+
+     def name_of(self):
+        print(f"name of this student is  {self.name}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    std = Man(name='rahul',age=18)
+    std.name_of()
+
+
+
+
+##
